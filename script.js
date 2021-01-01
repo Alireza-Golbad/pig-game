0fd6674c200c827a0f82c3ed7add4fbb55067645
Alireza Golbad <63424607+Alireza-Golbad@users.noreply.github.com>
2021-01-01 15:08:30 +0330

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+// Selecting elements -----------------------
+const score0El = document.getElementById("score-0");
+const score1El = document.getElementById("score-1");
+const diceEl = document.querySelector(".dice");
+
+// statrting conditions ---------------------------
+diceEl.classList.add("hidden");
